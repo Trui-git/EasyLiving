598e6b6200ae25ac7cf68f141ce9f14f6bfcef6c
tengrui <tengrui13@gmail.com>
2020-06-20 01:02:10 +0800

    create edit bilactivity

---
+++ app/src/main/java/com/example/EasyLiving/EditBillsActivity.java
@@ -0,0 +1,13 @@
+package com.example.EasyLiving;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditBillsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_bills);
+    }
+}
